bd6591210744bddc7f3eff023b13395e9b7b5282
Nomiwaleed <120721823+Nomiwaleed@users.noreply.github.com>
2023-01-15 15:00:58 +0500

    Linq Query basic

---
+++ Week4-lab task/Name of days of week-Linq query/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Name_of_days_of_week_Linq_query
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new String[] {"monday","tuesday", "wednesday", "thursday","friday","Saturday","sunday"};
+            var val = from week in array select week;
+            
+            foreach(var item in val)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Week4-lab task/greater than 20 number using linq query/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace greater_than_20_number_using_linq_query
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var array = new String[] { "20", "30", "10", "15", "50", "12", "80", "100", "800", "804" };
+            //var val = from number in array select number;
+
+            //foreach (var item in val)
+            //{
+            //    if(Convert.ToInt32(item) > 20)
+            //    {
+            //        Console.WriteLine(item);
+            //    }
+            //}
+            //Console.ReadKey();
+
+            var array = new int[] { 20, 30, 10, 15, 50, 12, 80, 100, 800, 804 };
+            //var val = array.Where(a => a > 20);
+            foreach (var item in array.Where(a => a > 20))
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
